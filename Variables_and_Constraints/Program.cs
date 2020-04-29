eeff5d60555b063eca807a78a174f385dddd1cc5
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-29 12:29:09 -0500

    finished with video clip

--- Variables_and_Constraints/Program.cs
+++ Variables_and_Constraints/Program.cs
@@ -6,7 +6,24 @@ namespace Variables_and_Constraints
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float totalPrice = 20.50f;
+            var character = 'A';
+            string sentence = "This is a sentence.";
+            bool isLearning = true;
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(sentence);
+            System.Console.WriteLine(isLearning);
+
+            // ===== Format String =====
+            System.Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            // output 0 225
+            System.Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            // output -3.4028235E+38 3.4028235E+38
+
+            // ===== Constant =====
+            const float Pi = 3.14f;
+            System.Console.WriteLine(Pi);
         }
     }
 }
