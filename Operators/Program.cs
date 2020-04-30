bf2ea1090bbeca3a336a8c2f4d4af6fd9707b21d
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-30 14:23:50 -0500

    finished section

---
+++ Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            // b is equal to a and then a is increased by 1
+            int b = a++;
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            // output a = 2, b = 1
+
+            int c = 1;
+            // d is incremented by 1 and then assigned to c
+            int d = ++c;
+            // output c = 2, d = 2
+            Console.WriteLine(c);
+            System.Console.WriteLine(d);
+        }
+    }
+}
