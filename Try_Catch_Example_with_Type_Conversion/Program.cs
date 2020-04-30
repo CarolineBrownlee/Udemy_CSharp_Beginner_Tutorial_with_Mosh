55485158a15d420308568d1e513139d8d4607926
Caroline Brownlee <brownleecaroline@gmail.com>
2020-04-30 10:55:47 -0500

    finished section

---
+++ Try_Catch_Example_with_Type_Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Try_Catch_Example_with_Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            // this block will be monitored
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                System.Console.WriteLine(b);
+            }
+            // and if an exception, or error occurs, this block will be executed, which prevents the application from crashing and instead displays a friendly message to the user
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("I'm sorry, the number can't be converted to a byte.");
+            }
+        }
+    }
+}
