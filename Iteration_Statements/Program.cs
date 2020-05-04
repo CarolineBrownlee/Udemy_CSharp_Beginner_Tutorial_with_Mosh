a9f0ddc9a1820727c59491db69ce4df1f6ed6116
Caroline Brownlee <brownleecaroline@gmail.com>
2020-05-04 08:54:57 -0500

    started section ready for demo

---
+++ Iteration_Statements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Iteration_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// ===== for LOOPS =====
+// for loops (initialization clause (counter); condition clause (will loop while condition is valid); iteration clause (increment, or decrement the couter variable))
+// for (var i = o; i < 10; i++)
+// {
+//  // statements that will execute 
+// }
+
+// ===== foreach LOOPS =====
+// foreach Loops are for lists, or arrays
+// foreach (var number in numbers)
+// {
+    // statements that will execute
+// }
+
+// ===== while LOOPS =====
+// while (as long as this condition is valid the loop will execute)
+// similar to for loop, but with different syntax
+// while (i < 10)
+// {
+    // statements that will execute
+    // ...
+    // i++;
+// }
+
+// ===== Do-While LOOPS =====
+// loop executed at least once because condtion evaluated at the end
+// don't always need a counter
+// do 
+// {
+//     ...
+//     i++;
+// } while (int < 10);
+
+// ==== BREAK AND CONTINUE =====
+// BREAK TO JUMP OUT OF THE LOOP
+// CONTINUE TO JUMP TO NEXT ITERATION
